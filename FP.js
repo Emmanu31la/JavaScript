3ff3545496d1d14a20a365e3aa9795d585ab66b3
Emmanu31la <emmanuellagodwithus26@gmail.com>
2026-04-21 15:42:02 +0100

    Add FP.js examples and Notes.txt
    Add FP.js containing commented functional-programming snippets (currency conversion, pure functions, object creation, recursion, and small helpers) for reference and experimentation. Add Notes.txt documenting comment-tag conventions (highlight, alert, question, TODO, normal).

---
+++ FP.js
@@ -0,0 +1,50 @@
+// * Functional Programing
+/* 
+var currencyOne = 100;
+var currencyTwo = 0;
+var exchangeRate = 1.2;
+
+function convertCurrency(amount, rate){
+    return amount * rate;
+}
+
+currencyTwo = convertCurrency(currencyOne, exchangeRate);
+console.log(currencyTwo);
+*/
+/*
+function consoleLog(val) {
+    console.log(val)
+    return val
+}
+
+consoleLog("Hey there, what's shaking?")
+
+function doubleIt(num) {
+    return num * 2
+}
+
+function objectMaker(val) {
+    return{
+        prop: val
+    }
+}
+
+objectMaker(20);
+
+doubleIt(10).toString();
+
+let result = objectMaker( doubleIt(100) );
+console.log(result)
+
+let counter = 3;
+function example() {
+    console.log(counter);
+    counter = counter - 1;
+    if (counter === 0) return;
+    example();
+}
+
+example();
+
+*/
+
